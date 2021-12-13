d04efb14ec12f901c442eb95d9434d92e52575b3
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2021-12-13 12:51:38 +0300

    added weekday alert on the js

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ toggleBar.addEventListener("click", function () {
 });
 
 function akhaName() {
+  var weekDay = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   var DD = parseInt(document.getElementById("date").value);
   var MM = parseInt(document.getElementById("month").value);
   var year = document.getElementById("year").value;
@@ -24,59 +33,67 @@ function akhaName() {
   switch (switchAnswer) {
     case "1male":
     case "-6male":
-      alert("Your Akan name is Kwasi");
+      alert("Your Akan name is Kwasi, implying you were born on " + weekDay[0]);
       break;
     case "1female":
     case "-6female":
-      alert("Your Akan name is Akosua");
+      alert(
+        "Your Akan name is Akosua, implying you were born on " + weekDay[0]
+      );
       break;
     case "2male":
     case "-5male":
-      alert("Your Akan name is Kwadwo");
+      alert(
+        "Your Akan name is Kwadwo, implying you were born on " + weekDay[1]
+      );
       break;
     case "2female":
     case "-5female":
-      alert("Your Akan name is Adwoa");
+      alert("Your Akan name is Adwoa, implying you were born on " + weekDay[1]);
       break;
     case "3male":
     case "-4male":
-      alert("Your Akan name is Kwabena");
+      alert(
+        "Your Akan name is Kwabena, implying you were born on " + weekDay[2]
+      );
       break;
     case "3female":
     case "-4female":
-      alert("Your Akan name is Abenaa");
+      alert(
+        "Your Akan name is Abenaa, implying you were born on " + weekDay[2]
+      );
       break;
     case "4male":
     case "-3male":
-      alert("Your Akan name is Kwaku");
+      alert("Your Akan name is Kwaku, implying you were born on " + weekDay[3]);
       break;
     case "4female":
     case "-3female":
-      alert("Your Akan name is Akua");
+      alert("Your Akan name is Akua, implying you were born on " + weekDay[3]);
       break;
     case "5male":
     case "-2male":
-      alert("Your Akan name is Yow");
+      alert("Your Akan name is Yow, implying you were born on " + weekDay[4]);
       break;
     case "5female":
     case "-2female":
-      alert("Your Akan name is Yaa");
+      alert("Your Akan name is Yaa, implying you were born on " + weekDay[4]);
       break;
     case "6male":
     case "-1male":
-      alert("Your Akan name is Kofi");
+      alert("Your Akan name is Kofi, implying you were born on " + weekDay[5]);
       break;
     case "6female":
     case "-1female":
-      alert("Your Akan name is Afua");
+      alert("Your Akan name is Afua, implying you were born on " + weekDay[5]);
       break;
     case "0male":
     case "-0male":
-      alert("Your Akan name is Kwame");
+      alert("Your Akan name is Kwame, implying you were born on " + weekDay[6]);
       break;
     case "0female":
     case "-0female":
-      alert("Your Akan name is Ama");
+      alert("Your Akan name is Ama, implying you were born on " + weekDay[6]);
       break;
     default:
       alert("oops! seems like you errored retry with correct inputs");
